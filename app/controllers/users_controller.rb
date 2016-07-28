9abc32d78df09fbf4d5dc1c652132c6aafd5cb95
Ali Al-Bahrani <alibahrani@mac.com>
2016-07-28 10:53:34 -0400

    Finish user Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
-  end
+  
+ 
+    def show
+        @user =User.find(params[:id])
+        
+    end
+    
+    def new
+        @user = User.new
+        
+    end
+    
+    def create 
+        @user = User.new(user_params) #not the final implemntation!
+        if @user.save
+            flash[:success] = "Welcome to Gazebo Burgers & Grill Team! "
+            redirect_to @user
+            #handle a sucessful save
+        else
+            render 'new'
+            end
+    end
+    
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    
 end
